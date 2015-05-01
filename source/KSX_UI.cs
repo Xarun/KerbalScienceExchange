da048993d4d8a9860b575f79f48e3fa566323494
Xarun <Xarun@users.noreply.github.com>
2015-05-01 18:07:05 +0200

    1.1
    v 1.1
    * Misc:
    * KSP 1.0 recompile

--- source/KSX_UI.cs
+++ source/KSX_UI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KerboKatz.Extensions;
+using System;
 using UnityEngine;
 
 namespace KerboKatz
@@ -9,36 +10,41 @@ namespace KerboKatz
     private GUIStyle numberFieldStyle;
     private GUIStyle textStyle;
     private GUIStyle windowStyle;
+    private Rectangle windowPosition = new Rectangle(Rectangle.updateType.Cursor);
+    private int windowID = 1702000200;
+    private GUIStyle toolbarOptionLabelStyle;
     public void OnGUI()
     {
       if (!initStyle) InitStyle();
 
-      if (currentSettings.getBool("showWindow"))
-      {
-        windowPosition = GUILayout.Window(6381183, windowPosition, MainWindow, "Kerbal Science Exchange", windowStyle);
-        Utilities.clampToScreen(ref windowPosition);
-        Utilities.showTooltip();
-      }
+      Utilities.UI.createWindow(currentSettings.getBool("showWindow"), windowID, ref windowPosition, MainWindow, "Kerbal Science Exchange", windowStyle);
+      Utilities.UI.showTooltip();
     }
+
     private void InitStyle()
     {
       windowStyle = new GUIStyle(HighLogic.Skin.window);
-      windowStyle.fixedWidth = 250f;
+      windowStyle.fixedWidth = 250;
       windowStyle.padding.left = 0;
 
       textStyle = new GUIStyle(HighLogic.Skin.label);
-      textStyle.fixedWidth = 190f;
+      textStyle.fixedWidth = 190;
       textStyle.margin.left = 10;
 
       numberFieldStyle = new GUIStyle(HighLogic.Skin.box);
-      numberFieldStyle.fixedWidth = 52f;
-      numberFieldStyle.fixedHeight = 22f;
+      numberFieldStyle.fixedWidth = 52;
+      numberFieldStyle.fixedHeight = 22;
       numberFieldStyle.alignment = TextAnchor.MiddleCenter;
       numberFieldStyle.padding.right = 7;
       numberFieldStyle.margin.top = 5;
 
       buttonStyle = new GUIStyle(GUI.skin.button);
       buttonStyle.fixedWidth = 75f;
+
+      if (Utilities.UI.sortTextStyle == null)
+        Utilities.UI.getTooltipStyle();
+      toolbarOptionLabelStyle = new GUIStyle(Utilities.UI.sortTextStyle);
+      toolbarOptionLabelStyle.padding.left += 6;
 
       initStyle = true;
     }
@@ -48,35 +54,37 @@ namespace KerboKatz
       GUILayout.BeginVertical();
       GUILayout.Space(25);
       GUILayout.BeginHorizontal();
-      Utilities.createLabel("Science amount:", textStyle);
+      Utilities.UI.createLabel("Science amount:", textStyle);
       sellBuyString = GUILayout.TextField(Utilities.getOnlyNumbers(sellBuyString), 5, numberFieldStyle);
       GUILayout.EndHorizontal();
       GUILayout.BeginHorizontal();
-      Utilities.createLabel("Conversion rate", textStyle);
-      Utilities.createLabel(currentSettings.getFloat("conversionRate").ToString("N0"), textStyle);
+      Utilities.UI.createLabel("Conversion rate", textStyle);
+      Utilities.UI.createLabel(currentSettings.getFloat("conversionRate").ToString("N0"), textStyle);
       GUILayout.EndHorizontal();
       GUILayout.BeginHorizontal();
-      Utilities.createLabel("Tax & conversion fee", textStyle, "The conversion fee depends on your current reputation(" + Math.Round(Reputation.CurrentRep).ToString("N0") + "). At " + currentSettings.getFloat("repHigh").ToString("N0") + " the conversion fee is 0% and at " + currentSettings.getFloat("repLow").ToString("N0") + " the fee is " + currentSettings.getString("delta") + "%");
-      Utilities.createLabel(Utilities.round(calcModifier(), 2).ToString(), textStyle);
+      Utilities.UI.createLabel("Tax & conversion fee", textStyle, "The conversion fee depends on your current reputation(" + Math.Round(Reputation.CurrentRep).ToString("N0") + "). At " + currentSettings.getFloat("repHigh").ToString("N0") + " the conversion fee is 0% and at " + currentSettings.getFloat("repLow").ToString("N0") + " the fee is " + currentSettings.getString("delta") + "%");
+      Utilities.UI.createLabel(Utilities.round(calcModifier(), 2).ToString(), textStyle);
       GUILayout.EndHorizontal();
       GUILayout.Space(12);
       GUILayout.BeginHorizontal();
       GUILayout.FlexibleSpace();
       float buyScienceAmount = (float)buyScience(true);
       float sellScienceAmount = (float)sellScience(false, true);
-      if (Utilities.createButton("Buy", buttonStyle, ("Buy science for " + Math.Round(buyScience(true))), !Funding.CanAfford(buyScienceAmount) || buyScienceAmount == 0))
+      if (Utilities.UI.createButton("Buy", buttonStyle, ("Buy science for " + Math.Round(buyScience(true))), !Funding.CanAfford(buyScienceAmount) || buyScienceAmount == 0))
       {
         buyScience();
       }
       GUILayout.FlexibleSpace();
-      if (Utilities.createButton("Sell", buttonStyle, ("Sell science for " + Math.Round(sellScience(true))), !ResearchAndDevelopment.CanAfford(sellScienceAmount) || sellScienceAmount == 0))
+      if (Utilities.UI.createButton("Sell", buttonStyle, ("Sell science for " + Math.Round(sellScience(true))), !ResearchAndDevelopment.CanAfford(sellScienceAmount) || sellScienceAmount == 0))
       {
         sellScience();
       }
       GUILayout.FlexibleSpace();
       GUILayout.EndHorizontal();
+      Utilities.UI.createOptionSwitcher("Use:", Toolbar.toolbarOptions, ref toolbarSelected, toolbarOptionLabelStyle);
+      updateToolbarBool();
       GUILayout.EndVertical();
-      Utilities.updateTooltipAndDrag();
+      Utilities.UI.updateTooltipAndDrag();
     }
   }
 }
--- source/KerbalScienceExchange.cs
+++ source/KerbalScienceExchange.cs
@@ -6,47 +6,40 @@ namespace KerboKatz
   [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
   public partial class KerbalScienceExchange : KerboKatzBase
   {
-    private bool initStyle       = false;
-    private double lastTime      = 0;
-    private double modifier      = 0;
-    private Rect windowPosition  = new Rect(0, 0, 0, 0);
+    private bool initStyle = false;
+    private double lastTime = 0;
+    private double modifier = 0;
     private string sellBuyString = "0";
 
     public KerbalScienceExchange()
     {
       modName = "KerbalScienceExchange";
-      requiresUtilities = new Version(1, 0, 4);
+      displayName = "Kerbal Science Exchange";
+      requiresUtilities = new Version(1, 2, 0);
     }
 
-    public override void Start()
+    protected override void Started()
     {
       if (!(HighLogic.CurrentGame.Mode == Game.Modes.CAREER || HighLogic.CurrentGame.Mode == Game.Modes.SCIENCE_SANDBOX))
       {
         Destroy(this);
         return;
       }
-      base.Start();
+
       currentSettings.setDefault("delta", "10");
       currentSettings.setDefault("repLow", "0");
       currentSettings.setDefault("repHigh", "10000");
       currentSettings.setDefault("conversionRate", "1000");
       currentSettings.setDefault("tax", "1");
       currentSettings.setDefault("showWindow", "false");
-      currentSettings.setDefault("windowX", "99999");
-      currentSettings.setDefault("windowY", "38");
       windowPosition.x = currentSettings.getFloat("windowX");
       windowPosition.y = currentSettings.getFloat("windowY");
+
+      setIcon(Utilities.getTexture("icon", "KerbalScienceExchange/Textures"));
+      setAppLauncherScenes(ApplicationLauncher.AppScenes.SPACECENTER);
     }
 
-
-    public override void OnGuiAppLauncherReady()
-    {
-      base.OnGuiAppLauncherReady();
-      button.Setup(toggleWindow, toggleWindow, Utilities.getTexture("icon", "KerbalScienceExchange/Textures"));
-      button.VisibleInScenes = ApplicationLauncher.AppScenes.SPACECENTER;
-    }
-
-    private void toggleWindow()
+    protected override void onToolbar()
     {
       if (currentSettings.getBool("showWindow"))
       {
@@ -57,7 +50,8 @@ namespace KerboKatz
         currentSettings.set("showWindow", true);
       }
     }
-    public override void OnDestroy()
+
+    protected override void OnDestroy()
     {
       if (currentSettings != null)
       {
@@ -79,7 +73,7 @@ namespace KerboKatz
       modifier = (double)(1 + (repScale * currentSettings.getFloat("delta") + currentSettings.getFloat("tax")) / 100);
       return modifier;
     }
-    
+
     private double sellScience(bool ret = false, bool retScience = false)
     {
       double sellScience;
@@ -96,7 +90,7 @@ namespace KerboKatz
           return 0;
         }
         ResearchAndDevelopment.Instance.AddScience((float)-sellScience, TransactionReasons.None);
-        Funding.Instance.AddFunds(addFunding, TransactionReasons.None);
+        Utilities.Funding.addFunds(addFunding);
         sellBuyString = "0";
       }
       return 0;
@@ -116,7 +110,7 @@ namespace KerboKatz
           return 0;
         }
         ResearchAndDevelopment.Instance.AddScience((float)buyScience, TransactionReasons.None);
-        Funding.Instance.AddFunds(-removeFunding, TransactionReasons.None);
+        Utilities.Funding.addFunds(-removeFunding);
         sellBuyString = "0";
       }
       return 0;
